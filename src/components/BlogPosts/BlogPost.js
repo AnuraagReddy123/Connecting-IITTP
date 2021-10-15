83c68fa32135a0e53be45169dfd55f82b0a26c46
Anuraag Reddy <anuraagreddy74@outlook.com>
2021-10-15 15:40:57 +0530

    Added header to blog page

---
+++ src/components/BlogPosts/BlogPost.js
@@ -0,0 +1,5 @@
+import classes from './BlogPost.module.css';
+
+export default function BlogPost() {
+  return <div>posts</div>;
+}
--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,8 +1,11 @@
+import BlogPost from '../components/BlogPosts/BlogPost';
+import Header from '../components/Header/Header';
 import classes from './BlogPage.module.css';
 
 function BlogPage() {
   return <div>
-    Hello
+    <Header />
+    <BlogPost />
   </div>
 }
 
