7a9651a5bd2bb848fe2c40aafd8ee7d6439474ae
Anuraag Reddy <anuraagreddy74@outlook.com>
2021-10-20 16:59:18 +0530

    Fixed package errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import Login from './components/signIn';
-import SignIn from './components/authentication';
-import Authentication from './components/authentication.js';
+import Authentication from './webPages/authentication.js';
 import Navbar from './components/Navbar';
 import Homepage from './components/Homepage';
-
+import { Route, Switch } from 'react-router';
+import BlogPage from './pages/BlogPage';
+import { BrowserRouter } from 'react-router-dom';
+import Buy from './components/Products/Buy';
+import Sell from './components/Products/Sell';
 /*
 function App() {
   return (
@@ -16,11 +18,6 @@ function App() {
 
     </div>
  */
-import { Route, Switch } from 'react-router';
-import BlogPage from './pages/BlogPage';
-import { BrowserRouter } from 'react-router-dom';
-import Buy from './components/Products/Buy';
-import Sell from './components/Products/Sell';
 
 function App() {
   return (
@@ -33,11 +30,11 @@ function App() {
         <Route path='/blogs'>
           <BlogPage />
         </Route>
-        <Route path="/buy">
-          <Buy/>
+        <Route path='/buy'>
+          <Buy />
         </Route>
-        <Route path="/sell">
-          <Sell/>
+        <Route path='/sell'>
+          <Sell />
         </Route>
       </Switch>
     </BrowserRouter>
