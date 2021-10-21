4d5ca42100e9236d1ec80cceeb086c2754a30d2b
Anuraag Reddy <anuraagreddy74@outlook.com>
2021-10-21 23:03:49 +0530

    Completed Write Blog page

--- src/pages/Blog/WriteBlog.js
+++ src/pages/Blog/WriteBlog.js
@@ -1,28 +1,33 @@
-import classes from './WriteBlog.module.css';
+import './WriteBlog.css';
 
 export default function WriteBlog() {
   return (
-    <div className={classes.write}>
-      <form className={classes.writeForm}>
-        <div className={classes.formGroup}>
+    <div className='write'>
+      <img
+        className='writeImg'
+        src='https://staticdelivery.nexusmods.com/images/952/16613024-1477159025.png'
+        alt=''
+      />
+      <form className='writeForm'>
+        <div className='writeFormGroup'>
           <label for='fileInput'>
-            <i class='fas fa-plus'></i>
+            <i class='writeIcon fas fa-plus' />
           </label>
           <input type='file' id='fileInput' style={{ display: 'none' }} />
           <input
             type='text'
             placeholder='Title'
-            className={classes.writeInput}
+            className='writeInput'
             autoFocus={true}
           />
         </div>
-        <div className={classes.formGroup}>
+        <div className='writeFormGroup'>
           <textarea
             placeholder='Add your ideas!'
-            className={classes.writeText}
+            className='writeInput writeText'
           />
         </div>
-        <button className={classes.writeSubmit}>Publish</button>
+        <button className='writeSubmit'>Publish</button>
       </form>
     </div>
   );
