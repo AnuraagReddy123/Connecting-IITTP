f09f42be8e8a0f447555f82930e9bdde64769f62
Kishor783 <gddmkishor@gmail.com>
2021-10-24 19:17:47 +0530

    Shooping and Food Category is implemented

--- src/pages/Travel/travelling.js
+++ src/pages/Travel/travelling.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import TravellingCard from "../components/travellingCard";
+import CategoryCard from "../../components/categoryCard";
 import "./travelling.css";
-import Vehicle from "./vehicles.js";
+import Vehicle from "./Vehicle/vehicles.js";
+import vehicleImage from "./Vehicle/travelling.jpg";
 
 function Travelling(props) {
-  const handleCard = () => {};
 
   return (
     <Router>
@@ -24,12 +24,12 @@ function Travelling(props) {
               <div className="container-fluid cardsList">
                 <div className="row">
                   <div className="col-md-3 card1">
-                    <Link to="/travelling/vehicles" className="travellingCardLink">
-                      <TravellingCard
-                        onClick={handleCard}
+                    <Link to="/travelling/vehicles" className="vehicleCardLink">
+                      <CategoryCard
                         cardName="No personal vehicle"
                         cardText="Some quick example text to build on the card title and make up the
           bulk of the card's content."
+                        cardImageLink = {vehicleImage}
                       />
                     </Link>
                   </div>
