e233ad44582de6264ac1d434866341939474a4f8
harshithakapa1 <harshithakapa@gmail.com>
2021-10-25 15:29:03 +0530

    Userprofile updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Authentication from './components/authentication';
 import Navbar from './components/Navbar';
 import Homepage from './components/Homepage';
@@ -12,39 +12,56 @@ import WriteBlog from './pages/Blog/WriteBlog';
 import Shopping from './pages/Shopping/shopping';
 import Travelling from './pages/Travel/travelling';
 
+import Pwdchange from './components/pwdchange';
+import Userprofile from './components/Userprofile';
+
 import Food from './pages/Food/food';
 
 function App() {
+
   return (
-    // <BrowserRouter>
-    //   <Navbar />
-    //   <Switch>
-    //     <Route path='/home' exact>
-    //       <Homepage />
-    //     </Route>
-    //     <Route path='/' exact>
-    //       <Authentication />
-    //     </Route>
-    //     <Route path='/blogs'>
-    //       <BlogPage />
-    //     </Route>
-    //     <Route path='/buy'>
-    //       <Buy />
-    //     </Route>
-    //     <Route path='/sell'>
-    //       <Sell />
-    //     </Route>
-    //     <Route path='/singleBlog'>
-    //       <SingleBlog />
-    //     </Route>
-    //     <Route path='/writeBlog'>
-    //       <WriteBlog />
-    //     </Route>
-    //   </Switch>
-    // </BrowserRouter>
-    // <Shopping/>
-    // <Travelling/>
-    <Food/>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path='/home' exact>
+          <Homepage />
+        </Route>
+        <Route path='/' exact>
+          <Authentication />
+        </Route>
+        <Route path='/blogs'>
+          <BlogPage />
+        </Route>
+        <Route path='/buy'>
+          <Buy />
+        </Route>
+        <Route path='/sell'>
+          <Sell />
+        </Route>
+        <Route path='/singleBlog'>
+          <SingleBlog />
+        </Route>
+        <Route path='/writeBlog'>
+          <WriteBlog />
+        </Route>
+        <Route path='/shopping'>
+        <Shopping/>
+        </Route>
+        <Route path='/travelling'>
+        <Travelling/>
+        </Route>
+        <Route path='/food'>
+        <Food/>
+        </Route>
+        <Route path='/pwdchange'>
+        <Pwdchange/>
+        </Route>
+        <Route path='/Userprofile'>
+        <Userprofile/>
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
   );
 }
 
