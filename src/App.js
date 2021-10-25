b5635ba2bb10687087b16664c49feabe61f46f24
harshithakapa1 <harshithakapa@gmail.com>
2021-10-25 13:02:06 +0530

    conflict resolved

--- src/App.js
+++ src/App.js
@@ -9,47 +9,6 @@ import Buy from './components/Products/Buy';
 import Sell from './components/Products/Sell';
 import SingleBlog from './pages/Blog/SingleBlog';
 import WriteBlog from './pages/Blog/WriteBlog';
-<<<<<<< HEAD
-import Userprofile from './components/Userprofile';
-import Pwdchange from './components/pwdchange';
-
-function App() {
-  return (
-    // <Login/>
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route path='/home' exact>
-          <Homepage />
-        </Route>
-        <Route path='/' exact>
-          <Authentication />
-        </Route>
-        <Route path='/blogs'>
-          <BlogPage />
-        </Route>
-        <Route path='/buy'>
-          <Buy />
-        </Route>
-        <Route path='/sell'>
-          <Sell />
-        </Route>
-        <Route path='/singleBlog'>
-          <SingleBlog />
-        </Route>
-        <Route path='/writeBlog'>
-          <WriteBlog />
-        </Route>
-        <Route path='/Userprofile'>
-          <Userprofile />
-        </Route>
-        <Route path='/pwdchange'>
-          <Pwdchange />
-        </Route>
-    
-      </Switch>
-    </BrowserRouter>
-=======
 import Shopping from './pages/Shopping/shopping';
 import Travelling from './pages/Travel/travelling';
 
@@ -86,7 +45,6 @@ function App() {
     // <Shopping/>
     // <Travelling/>
     <Food/>
->>>>>>> 1f84aacc736e0efde3f4f5204123bad8385838d7
   );
 }
 
