b2d9732490ef3609d3fa26e22e6ae7d47a0f9913
nmnsharma007 <nmnsharma20@gmail.com>
2021-12-01 14:21:50 +0530

    fixed url causing authentication issues

--- backend/client/src/components/firebase/context.js
+++ backend/client/src/components/firebase/context.js
@@ -3,6 +3,11 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { auth } from "./firebase";
 
+const port = process.env.PORT || 4000;
+let url = 'http://localhost:';
+if (process.env.NODE_ENV === 'production')
+  url = 'https://save-environment-iittp.herokuapp.com';
+else url = `http://localhost:${port}`;
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
@@ -22,7 +27,7 @@ export const AuthProvider = ({ children }) => {
     useEffect(() => {
         onAuthStateChanged(auth, async (auth_user) => {
             if(auth_user) {
-                const result = await axios.get("http://localhost:4000/users/findEmail",{params: {email: auth_user.email}});
+                const result = await axios.get(`${url}/findEmail`,{params: {email: auth_user.email}});
                 const user = result.data;
                 console.log("user changed");
                 console.log(user);
